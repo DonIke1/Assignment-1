84e2d0fcc1c288ddae8de3e996395c54a4ff60e6
DonIke1 <136355034+DonIke1@users.noreply.github.com>
2023-06-14 10:56:37 +0000

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,5 @@
+num = int(input("enter year: "))
+if num % 4 == 0:
+    print("is a leap year")
+else:
+    print("its not a leap year")    
---
+++ vote.py
@@ -0,0 +1,5 @@
+num = int(input("Enter birth year: "))
+if num <= 2005:
+    print("You can vote")
+else:
+    print("You cannot vote")
